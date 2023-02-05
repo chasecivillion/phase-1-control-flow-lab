5e3ef5f0d337082e042cc7416b0e67519497e387
Beau Kim <beaukim3@gmail.com>
2023-02-04 20:29:45 -0500

    code complete

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(feet){
+ 
+  const promoGreeting = "This one is on me!"
+  const greeting = "That will be twenty bucks."
+  const surgeGreeting = "I will gladly take your thirty bucks."
+  const noGreeting = "No can do."
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  let distanceTraveled = promoGreeting
+  if (feet <= 400) {
+    return distanceTraveled;
+  } else if (
+    feet <= 2000) {
+      return greeting;
+  } else if (
+    feet <= 2500) {
+      return surgeGreeting;
+  } else if (
+    feet > 2500) {
+      return noGreeting;
+    }  
+};
+scuberGreetingForFeet(feet);
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+
+  const yesRide = "Ok, sounds good."
+  const noRide = "No go."
+  return (city === "NYC" ? yesRide : noRide);
+};
+ternaryCheckCity(city);
+
+function switchOnCharmFromTip(tip){
+
+  const bigTip = "Thank you so much."
+  const littleTip = "Thank you."
+  const noTip = "Bye."
+
+  switch(tip) {
+    case "generous":
+      return bigTip
+    case "not as generous":
+      return littleTip
+    default:
+      return noTip
+  };
+};
+switchOnCharmFromTip(tip);
